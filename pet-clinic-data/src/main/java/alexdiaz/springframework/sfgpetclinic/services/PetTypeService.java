73efcf52646d01bc3ed611a29779be0dbc658ce5
lexx <jrlexx@hotmail.com>
2020-01-01 17:34:45 -0500

    added PetTypeService closes #29

---
+++ pet-clinic-data/src/main/java/alexdiaz/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package alexdiaz.springframework.sfgpetclinic.services;
+
+import alexdiaz.springframework.sfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long> {
+
+}
